84cbef9f43fc63d20f198adb59f8f5c74d22fbaa
seveibar <seveibar@gmail.com>
2026-08-07 14:00:32 -0700

    Add Pipeline 7 partially pre-routed dataset

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # dataset-srj28-partially-prerouted
-Dataset01 circuits solved with Pipeline 7 and deterministically reduced to 50% of their traces
+
+Partially pre-routed Simple Route JSON benchmarks derived from every circuit in
+[`tscircuit/autorouting-dataset-01`](https://github.com/tscircuit/autorouting-dataset-01).
+
+Each source circuit is fully solved with
+`AutoroutingPipelineSolver7_MultiGraph` from
+[`@tscircuit/capacity-autorouter`](https://github.com/tscircuit/tscircuit-autorouter).
+A seeded pseudo-random shuffle then removes 50% of the solved traces, rounded
+down. The remaining traces are pre-routed obstacles for benchmarking routers
+that must finish the board without disturbing valid existing routes.
+
+## Install
+
+Datasets are installed directly from GitHub and are not published to npm:
+
+```sh
+bun add https://github.com/tscircuit/dataset-srj28-partially-prerouted
+```
+
+## Use
+
+```ts
+import { circuit001 } from "@tscircuit/dataset-srj28-partially-prerouted"
+```
+
+The package exposes the same named circuit exports as dataset01. Generation
+details and per-circuit trace counts are recorded in `manifest.json`.
+
+## Regenerate and validate
+
+```sh
+bun install
+bun run generate
+bun run check
+```
+
+Generation is deterministic for the pinned source dataset, Pipeline 7 version,
+and selection seed in `scripts/generate-dataset.ts`.
---
+++ index.js
@@ -0,0 +1,255 @@
+export { default as circuit001 } from "./circuits/circuit001.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit002 } from "./circuits/circuit002.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit003 } from "./circuits/circuit003.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit004 } from "./circuits/circuit004.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit005 } from "./circuits/circuit005.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit006 } from "./circuits/circuit006.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit007 } from "./circuits/circuit007.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit010 } from "./circuits/circuit010.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit011 } from "./circuits/circuit011.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit012 } from "./circuits/circuit012.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit013 } from "./circuits/circuit013.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit014 } from "./circuits/circuit014.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit015 } from "./circuits/circuit015.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit018 } from "./circuits/circuit018.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit019 } from "./circuits/circuit019.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit020 } from "./circuits/circuit020.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit021 } from "./circuits/circuit021.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit100 } from "./circuits/circuit100.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit101 } from "./circuits/circuit101.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit102 } from "./circuits/circuit102.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit103 } from "./circuits/circuit103.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit104 } from "./circuits/circuit104.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit105 } from "./circuits/circuit105.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit106 } from "./circuits/circuit106.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit107 } from "./circuits/circuit107.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit108 } from "./circuits/circuit108.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit109 } from "./circuits/circuit109.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit110 } from "./circuits/circuit110.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit111 } from "./circuits/circuit111.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit112 } from "./circuits/circuit112.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit113 } from "./circuits/circuit113.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit114 } from "./circuits/circuit114.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit115 } from "./circuits/circuit115.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit116 } from "./circuits/circuit116.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit118 } from "./circuits/circuit118.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit119 } from "./circuits/circuit119.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit120 } from "./circuits/circuit120.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit121 } from "./circuits/circuit121.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit123 } from "./circuits/circuit123.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit124 } from "./circuits/circuit124.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit125 } from "./circuits/circuit125.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit126 } from "./circuits/circuit126.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit127 } from "./circuits/circuit127.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit128 } from "./circuits/circuit128.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit129 } from "./circuits/circuit129.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit131 } from "./circuits/circuit131.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit132 } from "./circuits/circuit132.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit133 } from "./circuits/circuit133.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit134 } from "./circuits/circuit134.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit135 } from "./circuits/circuit135.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit136 } from "./circuits/circuit136.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit137 } from "./circuits/circuit137.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit138 } from "./circuits/circuit138.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit139 } from "./circuits/circuit139.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit140 } from "./circuits/circuit140.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit141 } from "./circuits/circuit141.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit142 } from "./circuits/circuit142.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit143 } from "./circuits/circuit143.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit144 } from "./circuits/circuit144.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit145 } from "./circuits/circuit145.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit146 } from "./circuits/circuit146.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit147 } from "./circuits/circuit147.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit148 } from "./circuits/circuit148.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit149 } from "./circuits/circuit149.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit150 } from "./circuits/circuit150.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit151 } from "./circuits/circuit151.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit152 } from "./circuits/circuit152.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit155 } from "./circuits/circuit155.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit156 } from "./circuits/circuit156.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit157 } from "./circuits/circuit157.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit158 } from "./circuits/circuit158.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit162 } from "./circuits/circuit162.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit164 } from "./circuits/circuit164.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit165 } from "./circuits/circuit165.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit168 } from "./circuits/circuit168.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit172 } from "./circuits/circuit172.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit173 } from "./circuits/circuit173.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit174 } from "./circuits/circuit174.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit175 } from "./circuits/circuit175.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit176 } from "./circuits/circuit176.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit177 } from "./circuits/circuit177.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit178 } from "./circuits/circuit178.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit179 } from "./circuits/circuit179.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit180 } from "./circuits/circuit180.circuit.srj.json" with {
+  type: "json",
+}
+export { default as circuit181 } from "./circuits/circuit181.circuit.srj.json" with {
+  type: "json",
+}
